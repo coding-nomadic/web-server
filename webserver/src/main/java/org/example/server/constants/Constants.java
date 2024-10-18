7dc09df5c4dac26c5f58fc50f37150477787f9b9
Tenzin Dawa <tnzngdw@gmail.com>
2024-10-18 12:27:35 -0400

    Delete webserver directory

--- webserver/src/main/java/org/example/server/constants/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.example.server.constants;
-
-public class Constants {
-    public static final String PORT = "port";
-    public static final String MAX_THREADS = "maxThreads";
-    public static final String FILE_NAME = "config.json";
-}
--- webserver/src/main/java/org/example/server/service/ServerExecutor.java
+++
@@ -1,8 +0,0 @@
-package org.example.server.service;
-
-public interface ServerExecutor {
-    /**
-     * execute the server
-     */
-    void execute();
-}
